1e3acc90536f22a595a5305b7132072e86b3858f
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-03-01 16:25:58 +1030

    generate bindings correctly and help clean up output

--- compile-dump.ps1
+++ compile-dump.ps1
@@ -1,12 +1,18 @@
 scons compiledb=yes
+[Console]::Out.Flush() 
+start-sleep -seconds 1
 
 ./bin/godot.windows.editor.x86_64.exe --headless --dump-extension-api-with-docs
+[Console]::Out.Flush() 
+start-sleep -seconds 1
+
 ./bin/godot.windows.editor.x86_64.exe --headless --dump-gdextension-interface
+[Console]::Out.Flush() 
+start-sleep -seconds 1
 
 cp gdextension_interface.h ../godot-api-export/gdextension_interface.h
-[Console]::Out.Flush() 
 cp extension_api.json ../godot-api-export/extension_api.json
 
 cd ../godot-cpp
-scons generate_bindings=yes build_library=no
+scons gdextension_dir="../godot-api-export" generate_bindings=yes build_library=no
 cd ../godot
