4fdefe81ea4de25955f7e290d42801967581dbfe
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-03-01 13:21:17 +1030

    At beginnings of powershell script to build and dump extension bindings

---
+++ compile-dump.ps1
@@ -0,0 +1,8 @@
+scons compiledb=yes
+
+./bin/godot.windows.editor.x86_64.exe --headless --dump-extension-api-with-docs
+./bin/godot.windows.editor.x86_64.exe --headless --dump-gdextension-interface
+
+cp gdextension_interface.h ../godot-api-export/gdextension_interface.h
+[Console]::Out.Flush() 
+cp extension_api.json ../godot-api-export/extension_api.json
