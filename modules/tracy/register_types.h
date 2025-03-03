f310a69bf10bb583386c6105924a8ef2365de519
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-03-04 00:25:47 +1030

    add empty module linking to tracy

---
+++ modules/tracy/register_types.h
@@ -0,0 +1,9 @@
+#ifndef GODOT_TRACY_REGISTER_TYPES_H
+#define GODOT_TRACY_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_tracy_module(ModuleInitializationLevel p_level);
+void uninitialize_tracy_module(ModuleInitializationLevel p_level);
+
+#endif // GODOT_TRACY_REGISTER_TYPES_H
