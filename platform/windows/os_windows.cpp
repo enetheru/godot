23bd85490359b4d88bf155988991e144d43d7123
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-03-04 01:19:47 +1030

    add framemark

--- platform/windows/os_windows.cpp
+++ platform/windows/os_windows.cpp
@@ -80,6 +80,8 @@
 #include "drivers/gles3/rasterizer_gles3.h"
 #endif
 
+#include <thirdparty/tracy/tracy/Tracy.hpp>
+
 #ifdef DEBUG_ENABLED
 #pragma pack(push, before_imagehlp, 8)
 #include <imagehlp.h>
@@ -2071,6 +2073,8 @@ void OS_Windows::run() {
 	main_loop->initialize();
 
 	while (true) {
+		FrameMark;
+
 		DisplayServer::get_singleton()->process_events(); // get rid of pending events
 		if (Main::iteration()) {
 			break;
